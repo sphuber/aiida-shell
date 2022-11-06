a185219adf47476284c26aade0ba453ea67c7ded
Sebastiaan Huber <mail@sphuber.net>
2022-11-06 16:17:19 +0100

    Add the `ShellCode` data plugin (#20)
    The `ShellCode` data plugin is a subclass of the `InstalledCode` plugin
    from `aiida-core`. The new plugin is used by the `launch_shell_job`
    utility function whenever a code has to be created on the fly. This will
    serve to be able to query for these kinds of codes and to distinguish
    them from codes that are typically setup manually.

--- src/aiida_shell/__init__.py
+++ src/aiida_shell/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """AiiDA plugin that makes running shell commands easy."""
 from .calculations import ShellJob
+from .data import ShellCode
 from .engine import launch_shell_job
 from .parsers import ShellParser
 
