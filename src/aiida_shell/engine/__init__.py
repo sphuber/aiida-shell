bb34c349ba320b17563904434cf0c4405775e2f8
Sebastiaan Huber <mail@sphuber.net>
2023-10-31 10:45:49 +0100

    Refactor: Move module `engine.launchers.shell_job` to `launch`
    The nested structure of the `engine.launchers` module stems from the
    very first conception of `aiida-shell` when there was an implementation
    based on process functions, in addition to the `CalcJob` implementation.
    each of which would get its own launcher.
    
    Now that only the `ShellJob` is left and the only one that will be
    necessary, the nesting is no longer needed.

--- src/aiida_shell/engine/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Module for :mod:`aiida_shell.engine`."""
-from .launchers import launch_shell_job
-
-__all__ = ('launch_shell_job',)
--- src/aiida_shell/engine/launchers/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Module for :mod:`aiida_shell.engine.launchers`."""
-from .shell_job import launch_shell_job
-
-__all__ = ('launch_shell_job',)
